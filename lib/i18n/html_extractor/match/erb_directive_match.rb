12d2657ef1169209ea81f41d87c63adfccb617a6
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2021-10-27 17:41:23 +0800

    improve erb_directive_match regexp to cover more scenarios

--- lib/i18n/html_extractor/match/erb_directive_match.rb
+++ lib/i18n/html_extractor/match/erb_directive_match.rb
@@ -6,8 +6,13 @@ module I18n
           [/^([ \t]*link_to )(("[^"]+")|('[^']+'))/, '\1%s', 2],
           [/^([ \t]*link_to (.*),[ ]?title:[ ]?)(("[^"]+")|('[^']+'))/, '\1%s', 3],
           [/^([ \t]*content_for (.*),)[ ]*(("[^"]+")|('[^']+'))/, '\1%s', 3],
+          [/^([ \t]*react_component (.*),[ ]?placeholder:[ ]?)(("[^"]+")|('[^']+'))/, '\1%s', 3],
+          [/^([ \t]*text_field_tag (.*),[ ]?placeholder:[ ]?)(("[^"]+")|('[^']+'))/, '\1%s', 3],
+          [/^([ \t]*number_field_tag (.*),[ ]?placeholder:[ ]?)(("[^"]+")|('[^']+'))/, '\1%s', 3],
+          [/^([ \t]*select_tag (.*),[ ]?include_blank:[ ]?)(("[^"]+")|('[^']+'))/, '\1%s', 3],
           [/^([ \t]*[a-z_]+\.[a-z_]+_field (.*),[ ]?placeholder:[ ]?)(("[^"]+")|('[^']+'))/, '\1%s', 3],
           [/^([ \t]*[a-z_]+\.text_area (.*),[ ]?placeholder:[ ]?)(("[^"]+")|('[^']+'))/, '\1%s', 3],
+          [/^([ \t]*[a-z_]+\.text_field (.*),[ ]?placeholder:[ ]?)(("[^"]+")|('[^']+'))/, '\1%s', 3],
           [/^([ \t]*[a-z_]+\.submit )(("[^"]+")|('[^']+'))/, '\1%s', 2],
           [/^([ \t]*[a-z_]+\.label\s+\:[a-z_]+\,\s+)(("[^"]+")|('[^']+'))/, '\1%s', 2]
         ].freeze
@@ -26,7 +31,7 @@ module I18n
         def self.create(document, fragment_id)
           REGEXPS.map do |r|
             match = document.erb_directives[fragment_id].match(r[0])
-            new(document, fragment_id, match[r[2]][1...-1], r) if match && match[r[2]] && match[r[2]] =~ /("[\p{Han}]+")|('[\p{Han}]+')/
+            new(document, fragment_id, match[r[2]][1...-1], r) if match && match[r[2]] && match[r[2]] =~ /[\p{Han}]/
           end
         end
       end
