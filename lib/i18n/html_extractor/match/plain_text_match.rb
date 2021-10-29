5863bfbddf1c386e0cef237a895dc967efbce73a
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2021-10-29 17:22:51 +0800

    refactor plain_text_match to deal with multiple scenarios

--- lib/i18n/html_extractor/match/plain_text_match.rb
+++ lib/i18n/html_extractor/match/plain_text_match.rb
@@ -2,6 +2,11 @@ module I18n
   module HTMLExtractor
     module Match
       class PlainTextMatch < BaseMatch
+        REGEXPS = [
+          [/\A([\p{Han}]*[^\p{Han}]*[\p{Han}]*.*)(@@=[a-z0-9\-]+@@)\z/, '@@=%s@@ \2'],
+          [/([ \t\n]*@@=([a-z0-9\-_]+)@@[ \t\n]+)(([\w ]*)[\p{Han}]+)(\n.*)/m, '\1%s\5']
+        ].freeze
+
         def self.create(document, node)
           return nil if node.name.start_with?('script')
           node.text.split(/\@\@(=?)[a-z0-9\-_]+\@\@/).select { |t| t =~ /[\p{Han}]/ }.map! do |text|
@@ -13,9 +18,12 @@ module I18n
           self.key = pinyin_name_key
           document.erb_directives[key] = " #{translation_key_object} "
           if node.content =~ /@@=(?<inner_text>[a-z0-9\-_]+)@@/
-            node.content = node.content.gsub(/\A([\p{Han}]*[^\p{Han}]*[\p{Han}]*.*)(@@=[a-z0-9\-]+@@)\z/) do |matched_text|
-              "@@=#{key}@@ #{$2}"
+            REGEXPS.map do |r|
+              node.content = node.content.gsub(r[0], r[1] % translation_key_object)
             end
+            # node.content = node.content.gsub(/\A([\p{Han}]*[^\p{Han}]*[\p{Han}]*.*)(@@=[a-z0-9\-]+@@)\z/) do |matched_text|
+            #   "@@=#{key}@@ #{$2}"
+            # end
           else
             node.content = node.content.gsub(text, "@@=#{key}@@")
         end
